6bb174b288591b0f3badddd8fe19db48584b3aa5
P4ver <s.pales20@gmail.com>
2023-11-15 13:31:29 -0800

    hh

---
+++ main_f.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+stack_t *head = NULL;
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
